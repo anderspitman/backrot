371208603549418f3463a4e7271e82a90331a127
Anders Pitman <tapitman11@gmail.com>
2022-03-31 19:00:16 -0600

    Implement setting inPath and destDir

--- README.md
+++
@@ -1,2 +0,0 @@
-backrot is a simple backup script for creating 7 daily, 3 weekly, 11 monthly,
-and infinite yearly backups.
--- backrot.sh
+++ backrot.sh
@@ -2,41 +2,56 @@
 
 set -e
 
-echo "Hi there" > backup.dump
-
-mkdir -p daily weekly monthly yearly
-
-
 function backup {
 
-    dayOfYear=$1
-    dayOfMonth=$2
-    dayOfWeek=$3
-    monthOfYear=$4
+    inPath=$1
+    destDir=$2
+    dayOfYear=$3
+    dayOfMonth=$4
+    dayOfWeek=$5
+    monthOfYear=$6
+
+    mkdir -p $destDir/daily $destDir/weekly $destDir/monthly $destDir/yearly
 
     if (( $dayOfYear == 1 )); then
         timestamp=$(date --iso-8601=seconds)
-        mv backup.dump yearly/$timestamp.dump 
+        mv $inPath $destDir/yearly/$timestamp.dump 
     elif (( $dayOfMonth == 1 )); then
-        mv backup.dump monthly/$monthOfYear.dump 
+        mv $inPath $destDir/monthly/$monthOfYear.dump 
     elif (( $dayOfMonth == 8 )); then
-        mv backup.dump weekly/1.dump 
+        mv $inPath $destDir/weekly/1.dump 
     elif (( $dayOfMonth == 15 )); then
-        mv backup.dump weekly/2.dump 
+        mv $inPath $destDir/weekly/2.dump 
     elif (( $dayOfMonth == 22 )); then
-        mv backup.dump weekly/3.dump 
+        mv $inPath $destDir/weekly/3.dump 
     else
-        mv backup.dump daily/$dayOfWeek.dump
+        mv $inPath $destDir/daily/$dayOfWeek.dump
     fi
 }
 
-if [[ $# -eq 0 ]]; then
+function usage {
+    echo "Usage: backrot.sh IN_PATH OUT_DIR"
+}
+
+inPath=$1
+if [[ -z $inPath ]]; then
+    usage
+    exit 1
+fi
+
+destDir=$2
+if [[ -z $destDir ]]; then
+    usage
+    exit 1
+fi
+
+if [[ -z $3 ]]; then
     dayOfYear=$(date +%-j)
     dayOfMonth=$(date +%-d)
     dayOfWeek=$(date +%-u)
     monthOfYear=$(date +%-m)
 
-    backup $dayOfYear $dayOfMonth $dayOfWeek $monthOfYear
+    backup $inPath $destDir $dayOfYear $dayOfMonth $dayOfWeek $monthOfYear
 else
     backup $1 $2 $3 $4
 fi
